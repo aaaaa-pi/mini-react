e21ce2a890e5e194cfec666334553c954c40a212
aaaaa-pi <770358634@qq.com>
2024-06-19 21:35:09 +0800

    feat: 实现filter筛选功能

--- vite-runner-react/src/Todos.jsx
+++ vite-runner-react/src/Todos.jsx
@@ -1,36 +1,21 @@
 import React from "../core/React";
 
 export function Todos (){
-    const [todos,setTodos] = React.useState([
-        {
-            id: crypto.randomUUID(), // 浏览器创建的唯一标识
-            title:'吃饭',
-            status: 'active'
-        },
-        {
-            id: crypto.randomUUID(), // 浏览器创建的唯一标识
-            title:'吃饭',
-            status: 'active'
-        },
-        {
-            id: crypto.randomUUID(), // 浏览器创建的唯一标识
-            title:'吃饭',
-            status: 'active'
-        }
-    ])
+    const [filter,setFilter] = React.useState("all")
+    const [todos,setTodos] = React.useState([])
+    const [displayTodos,setDisplayTodos] = React.useState([])
     const [inputValue,setInputValue] = React.useState("")
     function handleAdd (){
         addTodo(inputValue)
         setInputValue("")
     }
+    function createTodo(title) {
+        return {id: crypto.randomUUID(),title,status: 'active'}
+    }
     function addTodo(title) {
         setTodos((todos) => [
             ...todos,
-            { 
-                id: crypto.randomUUID(),   
-                title,
-                status: 'active'
-            }
+            createTodo(title)
         ])
     }
     function removeTodo(id) {
@@ -59,15 +44,71 @@ export function Todos (){
         })
         setTodos(newTodos)
     }
+
+    function saveTodos(){
+        localStorage.setItem("todos",JSON.stringify(todos))
+    }
+
+    React.useEffect(() => {
+        if(filter === "all"){
+            setDisplayTodos(todos)
+        }else if(filter === "active"){
+            const newDisplayTodos = todos.filter((todo) => {
+                return todo.status === "active"
+            })
+            setDisplayTodos(newDisplayTodos)
+        }else if(filter === "done"){
+            const newDisplayTodos = todos.filter((todo) => {
+                return todo.status === "done"
+            })
+            setDisplayTodos(newDisplayTodos)
+        }
+    },[filter,todos])
     
+    React.useEffect(() => {
+        const rawTodos = localStorage.getItem("todos")
+        if(rawTodos){
+            setTodos(JSON.parse(rawTodos))
+        }
+    },[])
     return (
         <div>
             <h1>Todos</h1>
             <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
             <button onClick = {handleAdd}>add</button>
+            <button onClick = {saveTodos}>save</button>
+
+            <div>
+                <input 
+                    type="radio" 
+                    name="filter"
+                    id="all"
+                    checked={filter === "all"} 
+                    onClick={() => setFilter("all")}
+                />
+                <label htmlFor="all">all</label>
+
+                <input 
+                    type="radio" 
+                    name="filter"
+                    id="active"
+                    checked={filter === "active"} 
+                    onClick={() => setFilter("active")}
+                />
+                <label htmlFor="active">active</label>
+
+                <input 
+                    type="radio" 
+                    name="filter"
+                    id="done"
+                    checked={filter === "done"} 
+                    onClick={() => setFilter("done")}
+                />
+                <label htmlFor="done">done</label>
+            </div>
 
             <ul>
-                {...todos.map((todo) => {
+                {...displayTodos.map((todo) => {
                    return (
                         <li>
                             <TodoItem 
